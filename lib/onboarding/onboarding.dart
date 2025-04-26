9f44946f706b5b1078d0a8e60a6d6b1307eb9c39
ko <71692311+willyyypatootieee@users.noreply.github.com>
2025-04-26 19:43:40 +0700

    refactor: Simplify Onboarding widget to use StatelessWidget and improve layout

--- lib/onboarding/onboarding.dart
+++ lib/onboarding/onboarding.dart
@@ -1,34 +1,30 @@
 import 'package:flutter/material.dart';
 
-class Onboarding extends StatefulWidget {
-  const Onboarding({super.key});
+class Onboarding extends StatelessWidget {
+  const Onboarding({Key? key}) : super(key: key);
 
   @override
-  State<Onboarding> createState() => _HomeState();
-}
-
-class _HomeState extends State<Onboarding> {
-  @override
-  Widget build(context) {
-    return Directionality(
-      textDirection: TextDirection.ltr,
-      child: Scaffold(
-        backgroundColor: Colors.white,
-        body: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.center,
-
-            children: [
-              Image.asset(
-                'assets/images/onboarding.png',
-                width: 400,
-                height: 400,
-              ),
-
-              const SizedBox(height: 40),
-            ],
-          ),
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white, // Set the background color to white
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/onboarding.png',
+              width: 400,
+              height: 400,
+            ),
+            Text(
+              'The free, fun, and effective way to learn a language',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(onPressed: () {}, child: Text('Get Started')),
+          ],
         ),
       ),
     );
--- lib/splashscreen/splash_screen.dart
+++ lib/splashscreen/splash_screen.dart
@@ -42,6 +42,7 @@ class Splash extends State<SplashScreen> {
               const SizedBox(height: 40),
             ],
           ),
+          
         ),
       ),
     );
